1c0e90e143ad1c6eff2dd4cdc8972e7b9d6fb49d
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2020-01-08 18:24:26 +0500

    PHP Practice 1

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Document</title>
+    </head>
+    <body>
+        <h1><?php echo "How are you?"; ?></h1>
+        <?php 
+            /* Hi how are you
+            Where are you form */
+            # this is a commnet
+            echo "<h1>Hi and welcome back.</h1>";
+            echo "Welcome to Full Stack - Batch 1";
+        ?>
+    </body>
+</html>
